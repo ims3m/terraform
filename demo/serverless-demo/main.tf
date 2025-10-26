3124526e9236226919d8dabc0d3a987b076190af
“ims3ms” <skm28112001@gmail.com>
2025-10-26 18:15:36 +0530

    update s3_bucket provider

--- demo/serverless-demo/main.tf
+++ demo/serverless-demo/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-module "my_lambda_function" {
-  source = "./modules/lambda"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-tf-aws-s3-test-bkt"
+
+  versioning = {
+    enabled = false
+  }
+}
+
+output "s3_bucket_arn" {
+  description = "ARN for the s3 Bucket created."
+  value       = module.s3_bucket.s3_bucket_arn
 }
